7674d96a831fd82be895039ea40f4ef665da2a65
Thomas Wheeler <Thomas.T.Wheeler@aa.com>
2020-04-25 07:57:06 -0500

    parse(lexer, precedence) returns a Node not a List<Node>
    Incorrect understanding of the semantics of parse resulted
    in initial implementation returning a list. This method
    should return a single expression.

--- src/main/java/org/byteworks/parse/pratt/Parser.java
+++ src/main/java/org/byteworks/parse/pratt/Parser.java
@@ -1,6 +1,6 @@
 package org.byteworks.parse.pratt;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.BiFunction;
 
@@ -12,10 +12,13 @@ public class Parser {
     }
 
     public static class Node {
+    }
+
+    public static class ExpressionNode extends Node {
 
     }
 
-    public static class LiteralNode extends Node {
+    public static class LiteralNode extends ExpressionNode {
         private final String value;
         public LiteralNode(String value) { this.value = value; }
 
@@ -25,60 +28,64 @@ public class Parser {
         }
     }
 
-    public static class PlusNode extends Node {
-        private final List<Node> lhs;
-        private final List<Node> rhs;
-        public PlusNode(List<Node> lhs, List<Node> rhs) { this.lhs = lhs; this.rhs = rhs; }
+    public static class PlusNode extends ExpressionNode {
+        private final Node lhs;
+        private final Node rhs;
+        public PlusNode(Node lhs, Node rhs) { this.lhs = lhs; this.rhs = rhs; }
 
         @Override
         public String toString() {
-            return "(+ " + Parser.toString(lhs) + " " + Parser.toString(rhs) + ")";
+            return "(+ " + lhs.toString() + " " + rhs.toString() + ")";
         }
     }
 
-    public static class MinusNode extends Node {
-        private final List<Node> lhs;
-        private final List<Node> rhs;
-        public MinusNode(List<Node> lhs, List<Node> rhs) { this.lhs = lhs; this.rhs = rhs; }
+    public static class MinusNode extends ExpressionNode {
+        private final Node lhs;
+        private final Node rhs;
+        public MinusNode(Node lhs, Node rhs) { this.lhs = lhs; this.rhs = rhs; }
 
         @Override
         public String toString() {
-            return "(- " + Parser.toString(lhs) + " " + Parser.toString(rhs) + ")";
+            return "(- " + lhs.toString() + " " + rhs.toString() + ")";
         }
     }
 
-    public static class MultNode extends Node {
-        private final List<Node> lhs;
-        private final List<Node> rhs;
-        public MultNode(List<Node> lhs, List<Node> rhs) { this.lhs = lhs; this.rhs = rhs; }
+    public static class MultNode extends ExpressionNode {
+        private final Node lhs;
+        private final Node rhs;
+        public MultNode(Node lhs, Node rhs) { this.lhs = lhs; this.rhs = rhs; }
 
         @Override
         public String toString() {
-            return "(* " + Parser.toString(lhs) + " " + Parser.toString(rhs) + ")";
+            return "(* " + lhs.toString() + " " + rhs.toString() + ")";
         }
     }
 
-
-    public static class DivideNode extends Node {
-        private final List<Node> lhs;
-        private final List<Node> rhs;
-        public DivideNode(List<Node> lhs, List<Node> rhs) { this.lhs = lhs; this.rhs = rhs; }
+    public static class DivideNode extends ExpressionNode {
+        private final Node lhs;
+        private final Node rhs;
+        public DivideNode(Node lhs, Node rhs) { this.lhs = lhs; this.rhs = rhs; }
 
         @Override
         public String toString() {
-            return "(/ " + Parser.toString(lhs) + " " + Parser.toString(rhs) + ")";
+            return "(/ " + lhs.toString() + " " + rhs.toString() + ")";
         }
     }
 
     public List<Node> parse(Lexer lexer) {
-        return parse(lexer, 0);
+        return Collections.singletonList(parse(lexer, 0));
     }
 
-    private List<Node> parse(final Lexer lexer, final int precedence) {
-        List<Node> ast = new ArrayList<>();
+    /*
+      0   == entry
+      1,2 == +, -
+      3,4 == *, /
+     */
+    private Node parse(final Lexer lexer, final int precedence) {
+        Node node = null;
         Lexer.Token token = lexer.next();
         if (token instanceof Lexer.Number) {
-            ast.add(new LiteralNode(token.getChars()));
+            node = new LiteralNode(token.getChars());
         }
         while (true) {
             token = lexer.peek();
@@ -90,42 +97,40 @@ public class Parser {
                 break;
             }
             lexer.next();
-            List<Node> rhs = parse(lexer, precedencePair.right);
+            Node rhs = parse(lexer, precedencePair.right);
             if (token instanceof Lexer.Plus) {
-                ast = rebuild(this::buildPlus, ast, rhs);
+                node = rebuild(this::buildPlus, node, rhs);
             } else if (token instanceof Lexer.Minus) {
-                ast = rebuild(this::buildMinus, ast, rhs);
+                node = rebuild(this::buildMinus, node, rhs);
             } else if (token instanceof Lexer.Mult) {
-                ast = rebuild(this::buildMult, ast, rhs);
+                node = rebuild(this::buildMult, node, rhs);
             } else if (token instanceof Lexer.Divide) {
-                ast = rebuild(this::buildDivide, ast, rhs);
+                node = rebuild(this::buildDivide, node, rhs);
             } else {
                 throw new IllegalArgumentException("Could not parse " + token.getClass().getSimpleName() + "[" + token.getChars() + "]");
             }
         }
-        return ast;
+        return node;
     }
 
-    private List<Node> rebuild(final BiFunction<List<Node>, List<Node>, Node> ctor, final List<Node> ast, final List<Node> rhs) {
+    private Node rebuild(final BiFunction<Node, Node, Node> ctor, final Node ast, final Node rhs) {
         Node node = ctor.apply(ast, rhs);
-        List<Node> newAst = new ArrayList<>();
-        newAst.add(node);
-        return newAst;
+        return node;
     }
 
-    private PlusNode buildPlus(List<Node> lhs, List<Node> rhs) {
+    private PlusNode buildPlus(Node lhs, Node rhs) {
         return new PlusNode(lhs, rhs);
     }
 
-    private MinusNode buildMinus(List<Node> lhs, List<Node> rhs) {
+    private MinusNode buildMinus(Node lhs, Node rhs) {
         return new MinusNode(lhs, rhs);
     }
 
-    private MultNode buildMult(List<Node> lhs, List<Node> rhs) {
+    private MultNode buildMult(Node lhs, Node rhs) {
         return new MultNode(lhs, rhs);
     }
 
-    private DivideNode buildDivide(List<Node> lhs, List<Node> rhs) {
+    private DivideNode buildDivide(Node lhs, Node rhs) {
         return new DivideNode(lhs, rhs);
     }
 
